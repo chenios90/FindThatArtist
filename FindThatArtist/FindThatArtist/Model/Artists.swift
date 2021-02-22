5ec6ad7d3006a826c0a215e4b3e5d33454c98bd5
Carlos Henderson <carlos.hendersonenhanceit@gmail.com>
2021-02-22 08:21:48 -0800

    pushing changes to url session

---
+++ FindThatArtist/FindThatArtist/Model/Artists.swift
@@ -0,0 +1,45 @@
+//
+//  Artists.swift
+//  FindThatArtist
+//
+//  Created by Field Employee on 2/20/21.
+//
+
+import Foundation
+import UIKit
+
+struct Artist: Decodable{
+    let results: [ArtistDetails]
+    
+    enum CodingKeys: String, CodingKey{
+        case results
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+                self.results = try container.decodeIfPresent([ArtistDetails].self, forKey: .results)!
+    }
+}
+
+struct ArtistDetails: Decodable{
+    let artistName: String
+    let trackName: String
+    let releaseDate: String
+    let primaryGenreName: String
+    let trackPrice: String
+    
+    enum CodingKeys: String, CodingKey{
+        case artistName
+        case trackName
+        case releaseDate
+        case primaryGenreName
+        case trackPrice
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.artistName = try container.decode(String.self, forKey: .artistName)
+        self.trackName = try container.decode(String.self, forKey: .trackName)
+        self.releaseDate = try container.decode(String.self, forKey: .releaseDate)
+        self.primaryGenreName = try container.decode(String.self, forKey: .primaryGenreName)
+        self.trackPrice = try container.decode(String.self, forKey: .trackPrice)
+    }
+}
---
+++ FindThatArtist/FindThatArtist/Views/SearchViewController.swift
@@ -0,0 +1,81 @@
+//
+//  SearchViewController.swift
+//  FindThatArtist
+//
+//  Created by Field Employee on 2/20/21.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    var artistDetails : [Artist] = []
+    @IBOutlet weak var searchArtist: UITextField!
+    
+    @IBOutlet weak var artistTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.artistTable.dataSource = self
+
+    }
+    
+    func constructURL(artistName: String) -> String{
+        //https://itunes.apple.com/search?term=jack+johnson
+        let api: String = "https://itunes.apple.com/"
+        let endpoint: String = "search?term=\(artistName)"
+        let url = (api + endpoint)
+        
+        return url
+    }
+    
+    @IBAction func SearchArtist(_ sender: Any) {
+     
+            getArtistData()
+        }
+    
+    func getArtistData(){
+        let theArtist = searchArtist.text!
+       
+        
+        let url = constructURL(artistName: theArtist)
+        print(url)
+        URLSession.shared.dataTask(with: URL(string: url)!){
+                    [weak self](data, _, error) in guard let data = data else{
+                        return
+                    }
+                guard let artistData = try? JSONDecoder().decode([Artist].self, from: data)else{
+                    print("couldnt decode")
+                    return
+                    
+                }
+                self?.artistDetails = artistData
+                DispatchQueue.main.async {
+                    self?.artistTable.reloadData()
+                }
+                }.resume()
+    }
+    
+    
+}
+
+extension SearchViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return artistDetails.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let table = self.artistDetails[0].results[0]
+        cell.textLabel?.text = table.artistName
+        cell.textLabel?.text = table.trackName
+        cell.textLabel?.text = table.releaseDate
+        cell.textLabel?.text = table.primaryGenreName
+        cell.textLabel?.text = table.trackPrice
+            print(indexPath)
+            return cell
+        
+    }
+    
+    
+}
