bf46fd7535c2c4b68af5eec3000be32732d941ee
Carlos Henderson <carlos.hendersonenhanceit@gmail.com>
2021-02-24 07:10:18 -0800

    pushing tests

--- FindThatArtist/FindThatArtistTests/FindThatArtistTests.swift
+++ FindThatArtist/FindThatArtistTests/FindThatArtistTests.swift
@@ -6,7 +6,6 @@
 //
 
 import XCTest
-@testable import FindThatArtist
 
 class FindThatArtistTests: XCTestCase {
 
--- FindThatArtist/FindThatArtistTests/TDDTests.swift
+++ FindThatArtist/FindThatArtistTests/TDDTests.swift
@@ -8,10 +8,18 @@
 import Foundation
 import XCTest
 
-
 class Test{
     
+    let svc = SearchViewController()
+    
     func testEnteredTextFieldSearchValue(){
-        
+        let artistNameEntered: String = svc.searchArtist.text!
+        XCTAssertEqual(artistNameEntered,svc.searchArtist.text)
+    }
+    
+    func testDefaultHidden(){
+        XCTAssertEqual(svc.artistTable.isHidden, true)
+        XCTAssertEqual(svc.activitySpin.isHidden, true)
+        XCTAssertEqual(svc.newSearchButton.isHidden, true)
     }
 }
